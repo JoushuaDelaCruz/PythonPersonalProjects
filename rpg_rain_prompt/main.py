1974ffa6c12b79916ca849f8e4f5849228979636
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-08-18 21:35:47 -0700

    Started on the storyline

--- rpg_rain_prompt/main.py
+++ rpg_rain_prompt/main.py
@@ -1,5 +1,41 @@
+import time
+
+
+class Stage:
+    """
+    A class responsible for running the game; It will print text on the command-line.
+    This class has two main functions: start() and end().
+    """
+
+    def start(self):
+        """
+        Starts the game.
+        """
+        self._screen_writer("Welcome to the game!", 0.05)
+
+    def end(self):
+        """
+        Ends the game.
+        """
+        pass
+
+    @staticmethod
+    def _screen_writer(text: str, delay: float):
+        """
+        Prints each character in text one by one on the command-line. 
+        Higher delay means slower typing and vice versa.
+
+        @param text: The text to be printed.
+        @param delay: The delay between each character.
+        """
+        for character in text:
+            print(character, end="", flush=True)
+            time.sleep(delay)
+
+
 def main():
-    print("Hello World!")
+    stage = Stage()
+    stage.start()
 
 
 if __name__ == "__main__":
