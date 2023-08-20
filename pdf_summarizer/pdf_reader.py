7ed43a2df760223800765dc38b04c629740f3c89
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-08-19 21:36:13 -0700

    Created a text file reader

--- pdf_summarizer/pdf_reader.py
+++ pdf_summarizer/pdf_reader.py
@@ -1,4 +1,6 @@
 import abc
+from typing import TextIO
+
 import PyPDF2
 from PyPDF2 import PdfReader
 
@@ -45,6 +47,7 @@ class PDFReader(TextReader):
         """
         Reads the content of a pdf file.
 
+        @param file_path: the file to read from
         @raises TypeError: if the file is not a pdf
         @raises FileNotFound: if the file cannot be found
         """
@@ -82,3 +85,49 @@ class PDFReader(TextReader):
         for page in reader.pages:
             content = page.extract_text()
             self._content += content
+
+
+class TextFileReader(TextReader):
+    """
+    A class responsible for reading a text file.
+    """
+
+    def read(self, file_path: str) -> str:
+        """
+        Reads the content of a text file.
+
+        @param file_path: the file to read from
+        @raises TypeError: if the file is not a pdf
+        @raises FileNotFound: if the file cannot be found
+        """
+        self._file = file_path
+        self._check_file_type()
+        self._read_txt()
+        return self._content
+
+    def _check_file_type(self):
+        """
+        Checks if the given file is a text file.
+
+        @raises TypeError: if file is not a text file
+        """
+        if not self._file.endswith(".txt"):
+            raise TypeError("File must be a text file!")
+
+    def _read_txt(self):
+        """
+        Reads the content of the text and store all the text into the content variable.
+
+        @raise FileNotFound: if the file cannot be found.
+        """
+        with open(self._file, encoding="utf8") as txt:
+            self._read_pages(txt)
+
+    def _read_pages(self, file: TextIO):
+        """
+        Reads each pages of a given file and store all the text to the content variable.
+
+        @param file: the file to read the pages from, TextIO
+        """
+        for line in file:
+            self._content += line
